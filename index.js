a3ee3cfc11d1f148a6ee7d2f0e80b802eb731512
Anirach Mingkhwan <anirach.m@fitm.kmutnb.ac.th>
2023-03-03 15:24:45 +0700

    add css to front-end

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 
 const express = require('express');
 const axios = require('axios');
-const app = express();
 var bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
 const base_url = "http://node41091-noderest.proen.app.ruk-com.cloud";
 
 // Set the template engine
+app.set('views', path.join(__dirname, '/public/views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,10 +20,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Serve static files
 app.use(express.static(__dirname + '/public'));
 
+
 app.get("/", async (req, res) => {
     try {
         const response = await axios.get(base_url + '/books');
-        res.render("books", { books: response.data });
+        res.render("books" , { books: response.data });
     } catch (err) {
         console.error(err);
         res.status(500).send('Error');
